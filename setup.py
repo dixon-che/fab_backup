d6c5230bdca22cb6c413b87afa6140b82da5b3d5
Aleksey Radchenko <dixon.che@gmail.com>
2013-10-01 20:26:45 +0300

    added setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+setup(
+    name='fab_backup',
+    version='0.1',
+    url='https://github.com/dixon-che/fab_backup',
+    license='MIT',
+    author='Aleksey Radchenko',
+    author_email='dixon.che@gmail.com',
+    description='fab functions for backups db and files to remote server',
+    long_description=__doc__,
+    py_modules=[
+        'fab_backup'
+    ],
+    zip_safe=True,
+    install_requires=[
+        'fabric (>=1.4)',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
