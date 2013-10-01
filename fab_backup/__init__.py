fa1b56e15be1d850015e2153d121401089a5ff10
Aleksey Radchenko <dixon.che@gmail.com>
2013-10-01 20:40:07 +0300

    Added version

--- fab_backup/__init__.py
+++ fab_backup/__init__.py
@@ -1,2 +1,10 @@
 
 from fab_backup import *
+
+__author__      = 'Aleksey Radchenko'
+__license__     = 'Apache License 2.0'
+__version__     = '0.1'
+__maintainer__  = __author__
+__email__       = 'dixon.che@gmail.com'
+__url__         = 'https://github.com/dixon-che/fab_backup'
+__summary__     = __doc__
